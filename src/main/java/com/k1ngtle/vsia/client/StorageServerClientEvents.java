90d66f297d54e444d18afd0502fdd10165210aac
J0k3r$ <maxriddle12347@gmail.com>
2026-08-17 03:25:32 +0700

    Commit: Add firewall

--- src/main/java/com/k1ngtle/vsia/client/StorageServerClientEvents.java
+++ src/main/java/com/k1ngtle/vsia/client/StorageServerClientEvents.java
@@ -3,18 +3,19 @@ package com.k1ngtle.vsia.client;
 import com.k1ngtle.vsia.Vsia;
 import com.k1ngtle.vsia.client.renderer.StorageServerRenderer;
 import com.k1ngtle.vsia.client.renderer.NetworkSwitchRenderer;
+import com.k1ngtle.vsia.client.renderer.FirewallRenderer;
 import com.k1ngtle.vsia.signality.SignalityBlocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-// ADD THESE 3 IMPORTS:
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import com.k1ngtle.vsia.registry.ModMenuTypes;
 import com.k1ngtle.vsia.client.screen.StorageServerScreen;
 import com.k1ngtle.vsia.client.screen.NetworkSwitchScreen;
+import com.k1ngtle.vsia.client.screen.FirewallScreen;
 
 @Mod.EventBusSubscriber(modid = Vsia.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class StorageServerClientEvents {
@@ -24,14 +25,15 @@ public final class StorageServerClientEvents {
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(SignalityBlocks.STORAGE_SERVER_BE.get(), context -> new StorageServerRenderer());
         event.registerBlockEntityRenderer(SignalityBlocks.NETWORK_SWITCH_BE.get(), context -> new NetworkSwitchRenderer());
+        event.registerBlockEntityRenderer(SignalityBlocks.FIREWALL_BE.get(), context -> new FirewallRenderer());
     }
 
-    // ADD THIS NEW EVENT LISTENER:
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
             MenuScreens.register(ModMenuTypes.STORAGE_SERVER_MENU.get(), StorageServerScreen::new);
             MenuScreens.register(ModMenuTypes.NETWORK_SWITCH_MENU.get(), NetworkSwitchScreen::new);
+            MenuScreens.register(ModMenuTypes.FIREWALL_MENU.get(), FirewallScreen::new);
         });
     }
 }
